4e0b683c6f82dc808551f62399e3486c4808c903
nkatha1 <nkathapatience1@gmail.com>
2023-12-03 09:31:11 -0800

    pyth

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if not sentence:
-        sentence = None
-    if sentence:
-        sen_len = len(sentence)
-    else:
-        sen_len = 0
-    return (sen_len, sentence if not sentence else sentence[:1])
+def max_integer(my_list=[]):
+    new_list = []
+    if my_list:
+        my_list.sort(reverse=True)
+        return (my_list[0])
+    return (None)
