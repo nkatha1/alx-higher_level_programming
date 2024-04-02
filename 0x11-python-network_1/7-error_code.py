57548037692de05103e8b0af41a21cb48d38a996
nkatha1 <nkathapatience1@gmail.com>
2024-04-01 18:42:53 -0700

    python

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-"""Sends a request to the URL, and displays the body of the response"""
-import requests
-import sys
+
+"""
+    Python script that takes in a URL sends a request to the URL
+    displays the body of the response
+"""
+
 
 if __name__ == "__main__":
+    import requests
+    import sys
+
     url = sys.argv[1]
+
     r = requests.get(url)
-    if not r.ok:
+    if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
