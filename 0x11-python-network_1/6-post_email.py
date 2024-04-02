a794d9bcd42ff4dcadca481d6b267b427581efb5
nkatha1 <nkathapatience1@gmail.com>
2024-04-01 18:41:02 -0700

    python

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""Sends a POST request to the passed URL with the email as a parameter
-and finally displays the body of the response"""
-import requests
-import sys
+
+"""
+    a Python script that takes in a URL and an email address
+    sends POST request to the passed URL with the email as a param
+"""
+
 
 if __name__ == "__main__":
+    import requests
+    import sys
+
     url = sys.argv[1]
-    payload = {'email': sys.argv[2]}
-    r = requests.post(url, data=payload)
+    value = {'email': sys.argv[2]}
+
+    r = requests.post(url, value)
     print(r.text)
